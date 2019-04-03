c1d49dea04d0dc5c915acaba8d77ec380efa03df
poncoe <poncoe4mac@gmail.com>
2019-04-03 11:37:56 +0700

    Hello Commit

---
+++ src/Test.java
@@ -0,0 +1,12 @@
+public class Test {
+    private String nama;
+    
+    public void Test(String nama){
+        this.nama = nama;
+    }
+
+    public static void main(String[]agrs){
+        System.out.println("Testing Push!");
+    }
+
+}
